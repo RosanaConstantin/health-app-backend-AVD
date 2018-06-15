6a0a78110595888d6449941dae1f484905c94ee2
Rosana-Constantin <Rosana.12mai>
2018-06-15 19:06:38 +0300

    temperature slideshow

--- components/TemperaturePage.js
+++ components/TemperaturePage.js
@@ -1,76 +1,200 @@
 import React, { Component } from 'react';
-import {Text,View, Image, ImageBackground, StyleSheet, Button} from 'react-native';
+import {Text,View, Image, ImageBackground, StyleSheet, Button, TouchableHighlight} from 'react-native';
+import ImageSlider from 'react-native-image-slider';
+
+const images = [
+    {source: require('../assets/nose.png'), width: 100, height: 100},
+    {source: require('../assets/sneezing.png'), width: 100, height: 100},
+    {source: require('../assets/fatigue.png'), width: 100, height: 100},
+    {source: require('../assets/cough.png'), width: 100, height: 100},
+    {source: require('../assets/chills.png'), width: 100, height: 100},
+    {source: require('../assets/sore.png'), width: 100, height: 100},
+    {source: require('../assets/headache.png'), width: 100, height: 100},
+    {source: require('../assets/fever.png'), width: 100, height: 100},
+];
+
+export default class TemperaturePage extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            temperature: 0,
+            state:''
+        }
+    }
+
+    componentWillMount(){
+        this.setState({temperature: global.temperature})
+        if(global.temperature > 36.1 && global.temperature < 37.2){
+            this.setState({state: 'NORMAL TEMPERATURE'});
+        } else if (global.temperature < 36.1){
+            this.setState({state: 'LOWER TEMPERATURE'});
+        } else {
+            this.setState({state: 'HIGHER TEMPERATURE'});
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container} >
+
+                <View style={styles.anotherContainerForText}>
+                    <View style={{borderStyle:"solid", borderRadius:5, borderWidth:1,  width:250, height:150, padding:1, alignSelf:"center"}}>
+                        <Text style={{fontSize:60, color:"#000", textAlign:"center"}}> {this.state.temperature} °C</Text>
+                        <View
+                            style={{
+                                borderBottomColor: 'black',
+                                borderBottomWidth: 1,
+                                marginBottom:20
+
+                            }}
+                        />
+                        <Text style={{fontSize:20, color:"#000", textAlign:"center", alignSelf:"center"}}>{this.state.state}</Text>
+                    </View>
+
+                </View>
+
+                <View style={{flexDirection:"row"}}>
+                    <View style={{width:"60%", marginLeft:15}}>
+
+                        <Text style={{fontSize:20, color:"#000",marginTop:20,  textAlign:"center"}}> Normal limits   </Text>
+                        <Text style={{fontSize:20, color:"#000",marginBottom:20,  textAlign:"center"}}> 36.1°C - 37.2°C.  </Text>
+                        <Text style={{fontSize:20, color:"#000", textAlign:"center" }}>If you feel any of </Text>
+                        <Text style={{fontSize:20, color:"#000", textAlign:"center", marginBottom:20}}>these common symptoms </Text>
+                        <ImageSlider
+                           // loop
+
+                            autoPlayWithInterval={3000}
+                            images={images}
+                            style={{flex:0, backgroundColor:"transparent",width:250, height:300}}
+                            customSlide={({ index, item}) => (
+
+                                    <Image key={index} source={item.source} style={{ flex:1, width:300, justifyContent:"center"}} />
+
+                            )}
+                        />
+                        <Text style={{fontSize:20, color:"#000", textAlign:"center", marginTop:20 }}>contact your doctor! </Text>
+                    </View>
+                    <View style={styles.thermometerContainer}>
+                        <ImageBackground
+                            source={require('../assets/empty-thermometer.png')}
+                            style={styles.thermometer}
+                        >
+                            <View style={styles.SquareVariabilShapeView}></View>
+                            <View style={styles.SquareShapeView}></View>
+                            <View style={styles.CircleShapeView}></View>
+                        </ImageBackground>
+
+
+                    </View>
+                    <View style={styles.anotherContainer}>
+                        <Text style={styles.gradeO}>50</Text>
+                        <Text style={{fontSize:20, marginTop:10,}}>40</Text>
+                        <Text style={{fontSize:20, marginTop:7,}}>30</Text>
+                        <Text style={{fontSize:20, marginTop:7,}}>20</Text>
+                        <Text style={{fontSize:20, marginTop:9,}}>10</Text>
+                        <Text style={styles.gradeT}>0</Text>
+                        <Text style={{fontSize:20, marginTop:10,}}>-10</Text>
+                        <Text style={{fontSize:20, marginTop:7,}}>-20</Text>
+                        <Text style={{fontSize:20, marginTop:7,}}>-30</Text>
+                        <Text style={{fontSize:20, marginTop:9,}}>-40</Text>
+                        <Text style={styles.gradeTr}>-50</Text>
+                    </View>
+
+                </View>
+
+            </View>
+        );
+    }
+}
+global.temperature = 40;
+const height = 3.5 * global.temperature;
+const marginTop = 215-height;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection:"row",
-       // justifyContent:"space-around",
+        flexDirection:"column",
         backgroundColor:"#ffe0d9"
     },
     thermometer: {
-       width:114,
-       height:500,
-       justifyContent:"flex-start",
+        width:114,
+        height:500,
+        justifyContent:"flex-start",
         marginLeft:6
+    },
+    buttons: {
+        zIndex: 1,
+        height: 15,
+        marginTop: -25,
+        marginBottom: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    customSlide: {
+        backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    customImage: {
+        width: 100,
+        height: 100,
     },
     thermometerContainer: {
         width: 120,
-        marginLeft: 50,
+        // marginLeft: 50,
         height:500,
-        marginTop:100,
-          backgroundColor:"#fff"
+        marginTop:20,
     },
     anotherContainer:{
-      marginLeft:-50
+        marginLeft:-35
     },
     CircleShapeView: {
         width:68,
         height:68,
         borderRadius:34,
         backgroundColor:"#f90e0f",
-        marginTop:425,
-        marginLeft:3
+        // marginTop:426,
+        marginTop:-5,
+        marginLeft:4,
+    },
+    SquareShapeView: {
+        width:32,
+        height:217,
+        backgroundColor:"#f90e0f",
+        //marginTop:209,
+        marginLeft:22,
+    },
+    SquareVariabilShapeView:{
+        width:32,
+        backgroundColor:"#f90e0f",
+        marginTop,
+        marginLeft:22,
+        height,
     },
     gradeO: {
         fontSize:30,
-        marginTop:50,
+        marginTop:20,
 
     },
     gradeT: {
         fontSize:30,
-        marginTop:160,
-        //marginLeft:50
+        // marginTop:135,
+        marginTop:-3,
+        marginLeft:10
 
     },
     gradeTr:{
         fontSize:30,
-        marginTop:160
+        // marginTop:135,
+        marginTop:-4,
+        marginLeft: -8
+    },
+    anotherContainerForText:{
+        alignSelf:"center",
+        marginTop:20
+        // marginTop: 200,
+        // marginLeft: 25
     }
 });
-
-
-export default class TemperaturePage extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.thermometerContainer}>
-                    <ImageBackground
-                        source={require('../assets/empty-thermometer.png')}
-                        style={styles.thermometer}
-                    >
-                      <View style={styles.CircleShapeView}></View>
-
-                    </ImageBackground>
-
-
-                </View>
-                <View style={styles.anotherContainer}>
-                    <Text style={styles.gradeO}>50</Text>
-                    <Text style={styles.gradeT}>0</Text>
-                    <Text style={styles.gradeTr}>-50</Text>
-                </View>
-            </View>
-        );
-    }
-}
